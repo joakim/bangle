69117ea0fc365c247d3b9b4c938ad6f0bb2c66b6
Joakim Stai <joakim@users.noreply.github.com>
2020-05-01 20:46:19 +0200

    Update app.js

--- apps/calendar360/app.js
+++ apps/calendar360/app.js
@@ -1,13 +1,3 @@
-// Show launcher on BTN2 long-press
-setWatch(
-  (e) => {
-    let duration = e.time - e.lastTime
-    if (duration >= 0.15) Bangle.showLauncher()
-  },
-  BTN2,
-  { repeat: false, edge: 'falling' }
-)
-
 const storage = require('Storage')
 const calendar = require('calendar.js')
 
@@ -21,6 +11,6 @@ g.clear()
   .drawString(day, 64, 64 + 25)
 
 // Save to storage for use by widget
-let settings = storage.readJSON('calendar360.json', 1) || {}
-settings.today = day
-storage.writeJSON('calendar360.json', settings)
+let data = storage.readJSON('calendar360.json', 1) || {}
+data.today = day
+storage.writeJSON('calendar360.json', data)
