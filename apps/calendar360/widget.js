b1c7da7ca8eded442c5ab86404c2f1ef450f973e
Joakim Stai <joakim@users.noreply.github.com>
2020-05-01 16:02:13 +0200

    Update widget.js

--- apps/calendar360/widget.js
+++ apps/calendar360/widget.js
@@ -1,31 +1,30 @@
-(() => {
-  const calendar = require('calendar')
+;(() => {
+  const calendar = require('calendar.js')
 
   let width = 25
 
   function draw() {
     let now = new Date()
-    let date = calendar.joakim_from_gregorian([
-      now.getFullYear(),
-      now.getMonth() + 1,
-      now.getDate(),
-    ])
-
+    let date = calendar.convert(now)
     let output = 'XABCDX'[date.quarter] + date.dayOfQuarter
 
     g.reset()
     g.setFont('Vector', 20)
     g.setFontAlign(0, 0)
     g.drawString(output, this.x + width / 2, this.y + 10)
+
+    // Redraw next midnight
+    let tomorrow = new Date()
+    tomorrow.setHours(0, 0, 0, 0)
+    tomorrow.setDate(tomorrow.getDate() + 1)
+    setTimeout(function () {
+      WIDGETS['calendar360'].draw(WIDGETS['calendar360'])
+    }, tomorrow - now)
   }
 
-  setInterval(function() {
-    WIDGETS['calendar360'].draw(WIDGETS['calendar360'])
-  }, 60 * 60 * 1000)
-
-  WIDGETS["calendar360"] = {
+  WIDGETS['calendar360'] = {
     area: 'tl',
     width: width,
-    draw: draw
+    draw: draw,
   }
 })()
