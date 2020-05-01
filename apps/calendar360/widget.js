88b21da7dd8c7b5b8061a4a70168803932f8b058
Joakim Stai <joakim@users.noreply.github.com>
2020-05-01 20:43:59 +0200

    Update widget.js

--- apps/calendar360/widget.js
+++ apps/calendar360/widget.js
@@ -3,29 +3,30 @@
 
   let width = 25
 
-  function redraw() {
-    WIDGETS['calendar360'].draw(WIDGETS['calendar360'])
-  }
+  // function redraw() {
+  //   WIDGETS['calendar360'].draw(WIDGETS['calendar360'])
+  // }
 
   function draw() {
-    let settings = storage.readJSON('calendar360.json', 1) || { today: '...' }
+    let data = storage.readJSON('calendar360.json', 1) || { today: '...' }
 
     g.reset()
     g.setFont('Vector', 20)
     g.setFontAlign(0, 0)
-    g.drawString(settings.today, this.x + width / 2, this.y + 10)
+    g.drawString(data.today, this.x + width / 2, this.y + 10)
 
     // Redraw every 10 minutes
-    setInterval(redraw, 10 * 60 * 1000)
+    setInterval(
+      () => WIDGETS['calendar360'].draw(WIDGETS['calendar360']),
+      10 * 60 * 1000
+    )
 
     // and why not next midnight
-    /*
-    let now = new Date()
-    let tomorrow = new Date()
-    tomorrow.setHours(0, 0, 0, 0)
-    tomorrow.setDate(tomorrow.getDate() + 1)
-    setTimeout(redraw, tomorrow - now)
-    */
+    // let now = new Date()
+    // let tomorrow = new Date()
+    // tomorrow.setHours(0, 0, 0, 0)
+    // tomorrow.setDate(tomorrow.getDate() + 1)
+    // setTimeout(redraw, tomorrow - now)
   }
 
   WIDGETS['calendar360'] = {
