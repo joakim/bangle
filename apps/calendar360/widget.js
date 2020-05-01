787b33f336db1d7aea0dbf5f2c1a5a32023633a3
Joakim Stai <joakim@users.noreply.github.com>
2020-05-01 20:08:58 +0200

    Update widget.js

--- apps/calendar360/widget.js
+++ apps/calendar360/widget.js
@@ -1,25 +1,28 @@
 ;(() => {
-  const calendar = require('calendar.js')
+  const storage = require('Storage')
 
   let width = 25
 
+  function redraw() {
+    WIDGETS['calendar360'].draw(WIDGETS['calendar360'])
+  }
+
   function draw() {
-    let now = new Date()
-    let date = calendar.convert(now)
-    let output = 'XABCDX'[date.quarter] + date.dayOfQuarter
+    let settings = storage.readJSON('calendar360.json', 1) || { today: '...' }
 
     g.reset()
     g.setFont('Vector', 20)
     g.setFontAlign(0, 0)
-    g.drawString(output, this.x + width / 2, this.y + 10)
+    g.drawString(settings.today, this.x + width / 2, this.y + 10)
 
-    // Redraw next midnight
+    // Redraw every 10 minutes
+    setInterval(redraw, 10 * 60 * 1000)
+
+    // and why not next midnight
     let tomorrow = new Date()
     tomorrow.setHours(0, 0, 0, 0)
     tomorrow.setDate(tomorrow.getDate() + 1)
-    setTimeout(function () {
-      WIDGETS['calendar360'].draw(WIDGETS['calendar360'])
-    }, tomorrow - now)
+    setTimeout(redraw, tomorrow - now)
   }
 
   WIDGETS['calendar360'] = {
