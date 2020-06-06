285a418720c2b88485fe1d64c5cc26bb66e11236
Joakim Stai <joakim@users.noreply.github.com>
2020-06-06 10:31:29 +0000

    Create util.js

---
+++ apps/clock360/util.js
@@ -0,0 +1,31 @@
+const systemSettings = storage.readJSON('setting.json', 1) || { timezone: 0 }
+
+exports.settings = storage.readJSON('clock360.json', 1) || {
+  timezone: 0,
+  division: 0,
+  origin: 270,
+  sun: false,
+  lat: 0,
+  lon: 0,
+  menuButton: 22,
+}
+
+exports.getMidnight = (now) => {
+  let midnight = new Date(now).setHours(
+    0, // In the midnight hour
+    systemSettings.timezone * 60, // 24-hour timezone (in minutes)
+    -(settings.timezone * 240), // 360-degree timezone (in seconds)
+    0
+  )
+
+  // Correct the date if it's off by one calendar day
+  if (now - midnight > 86400000) {
+    midnight += 86400000
+  } else if (now - midnight < 0) {
+    midnight -= 86400000
+  }
+
+  return midnight
+}
+
+
