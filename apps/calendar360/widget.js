57a287bbc6d09bc81b12a6bc7999872942d9d76b
Joakim Stai <joakim@users.noreply.github.com>
2020-06-06 09:59:17 +0000

    Update widget.js

--- apps/calendar360/widget.js
+++ apps/calendar360/widget.js
@@ -1,78 +1,84 @@
 ;(() => {
-  let size = 18
-  let width = 30
-  let timer
+  const util = require('cal360.util.js')
 
-  const NEWYEAR_TABLE = [
-    1900281600000, // 2030
-    1868659200000, // 2029
-    1837123200000, // 2028
-    1805587200000, // 2027
-    1774051200000, // 2026
-    1742428800000, // 2025
-    1710892800000, // 2024
-    1679356800000, // 2023
-    1647820800000, // 2022
-    1616198400000, // 2021
-    1584662400000, // 2020
-  ]
+  // More RAM and CPU efficient calendar calculation using a lookup table
+  const YEAR_12020 = 15846624
+  const YEAR_TABLE = new Uint32Array([
+    3156192, // 12030
+    2839968, // 12029
+    2524608, // 12028
+    2209248, // 12027
+    1893888, // 12026
+    1577664, // 12025
+    1262304, // 12024
+    946944, // 12023
+    631584, // 12022
+    315360, // 12021
+    0, // 12020
+  ])
+
+  let timer
+  let size = 18
 
   function date360(ts) {
-    let yearIndex = NEWYEAR_TABLE.findIndex((newyear) => ts > newyear)
-    let date = {
-      year: 2030 - yearIndex,
-      dayOfYear: Math.ceil((ts - NEWYEAR_TABLE[yearIndex]) / 86400000),
-    }
-    let quarter = Math.ceil(date.dayOfYear / 91)
-    if (quarter < 5) {
-      date.quarter = quarter
-      date.dayOfQuarter = date.dayOfYear - (quarter - 1) * 91 - 1
-      if (date.dayOfQuarter > 0) {
-        date.dayOfCalendarYear = date.dayOfYear - date.quarter
-        date.octant = (quarter - 1) * 2 + Math.ceil(date.dayOfQuarter / 45) || 1
-        date.dayOfOctant = date.dayOfQuarter % 45 || 45
-        date.nonad = Math.ceil(date.dayOfCalendarYear / 9)
-        date.dayOfNonad = date.dayOfCalendarYear % 9 || 9
-      }
-    } else {
-      date.quarter = 0
-      date.transition = date.dayOfYear - 364
-    }
-    return date
+    let yearIndex = YEAR_TABLE.findIndex((newyear) => {
+      return ts > (YEAR_12020 + newyear) * 100000
+    })
+
+    return util.expand({
+      year: 2030 - yearIndex + 10000,
+      dayOfYear: Math.ceil(
+        (ts - (YEAR_12020 + YEAR_TABLE[yearIndex]) * 100000) / 86400000
+      ),
+    })
   }
 
-  function draw() {
+  function startTimer() {
     let now = new Date().getTime()
+    // todo Adjust for 360 timezone
     let midnight = new Date(now).setHours(24, 0, 0, 0)
 
     // Redraw at midnight
     timer = setTimeout(() => {
-      draw()
+      draw(WIDGETS['cal360qrt'], midnight)
+      draw(WIDGETS['cal360oct'], midnight)
     }, midnight - now)
 
-    let date = date360(now)
+    // Draw now
+    draw(WIDGETS['cal360qrt'], now)
+    draw(WIDGETS['cal360oct'], now)
+  }
+
+  function draw(widget, ts) {
+    let date = date360(ts || new Date().getTime())
 
     g.reset()
       .setFont('Vector', size)
       .setFontAlign(0, 0)
       .setColor('#00aaff')
       .drawString(
-        `${date.dayOfOctant}/${date.octant}`,
-        this.x + width / 2,
+        widget.format(date),
+        this.x + widget.width / 2,
         this.y + size / 2
       )
   }
 
   Bangle.on('lcdPower', (on) => {
     if (timer) timer = clearTimeout(timer)
-    if (on) draw()
+    if (on) startTimer()
   })
 
-  WIDGETS['calendar360'] = {
-    area: 'tl',
-    width: width,
+  WIDGETS['cal360oct'] = {
+    area: 'bl',
+    width: 30,
     draw: draw,
+    format: (date) => `${date.dayOfOctant}/${date.octant}`,
   }
 
-  if (Bangle.isLCDOn) draw()
+  WIDGETS['cal360qrt'] = {
+    area: 'br',
+    width: 25,
+    draw: draw,
+    format: (date) => `${'XABCD'[date.quarter]}${date.dayOfQuarter}`,
+  }
 })()
